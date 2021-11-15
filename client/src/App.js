008d08b0f78efde7128645c12454f47dcaac5c73
NielsPoldervaart <Niels@meghira.nl>
2021-11-15 14:26:01 +0100

    Added frontend pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Template from './pages/Template';
+import Templates from './pages/Templates';
 
 function App() {
 
-  const [data, setData] = useState([{}])
+  // const [data, setData] = useState([{}])
 
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
+  // useEffect(() => {
+  //   fetch("/members").then(
+  //     res => res.json()
+  //   ).then(
+  //     data => {
+  //       setData(data)
+  //       console.log(data)
+  //     }
+  //   )
+  // }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Templates />}/>
+        <Route path="/template" element={<Template />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+    </Router>
   );
 }
 
