6ce80e5b8c18499fc8dcc455f3c14e0545df9689
NielsPoldervaart <Niels@meghira.nl>
2021-11-16 17:18:31 +0100

    Changed Template.js to get and display data from backend

--- client/src/pages/Template.js
+++ client/src/pages/Template.js
@@ -1,5 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router';
+
 const Template = () => {
-    return <h1>Template</h1>;
+    const { company_id, template_id } = useParams()
+    
+    const [template, setTemplate] = useState()
+
+    useEffect(() => {
+        fetch(`/template/${company_id}/${template_id}`).then(
+            res => res.json()
+          ).then(
+            data => {
+              setTemplate(data)
+            }
+          )
+    }, [company_id, template_id])
+
+    return (
+        <div>
+            <h1>Template - {template_id}</h1>
+            <p>
+                Company Name = {template.company_name} - Company ID = {template.company_id}
+                <br/>
+                File Path = {template.Template_file} - Template ID = {template.template_id}
+                <br/>
+            </p>
+        </div>
+    );
 };
   
 export default Template;
