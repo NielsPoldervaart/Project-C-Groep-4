81e357cfd06fbda808bd3763c93debb844da4b07
LuennaCL <luenna.ceri.landman@gmail.com>
2021-11-16 18:49:13 +0100

    login pagina + linkjes naar "wachtwoord vergeten" en "account aanvragen"

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,55 @@
+import './Login.css';
+
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <div className="login">
+            <div className="kyndaBackground">
+                <div className="gradientWrapper">
+                    <div className="linear-gradient"></div>
+                    <div className="radial-gradient"></div>
+                </div>
+            </div>
+
+            <div className="loginBox">
+                <div className="logoBox">
+                    <img src="https://excelsiorrotterdam.nl/wp-content/uploads/2018/01/kynda_web.png" alt="kyndaLogo" width="250" />    
+                </div>
+
+
+                <form className="loginForm" method="post" action id="form">
+                    <div className="loginInput">
+                        <div className="email">
+                            <div className="inputWrapper">
+                                <label className="inputLabel" for="email">
+                                E-mail
+                                </label>
+                                <input className="inputInput" type="text" name="email" id="email" required="required" maxLength="20" tabIndex="1" autoFocus="autofocus" />
+                            </div>
+                        </div>
+                        <div className="password">
+                            <div className="inputWrapper">
+                                <label className="inputLabel" for="password">
+                                Wachtwoord
+                                </label>
+                                <input className="inputInput" type="password" name="password" id="password" required="required" maxLength="20" tabIndex="2" autoComplete="off" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loginButton">
+                        <button className="button" type="submit" id="button">
+                            <span className="buttonLabel">Inloggen</span>
+                        </button>
+                    </div>
+                    <div className="register">
+                        <a className="registerLink" href="./register">Account aanvragen</a>
+                    </div>
+                    <div className="forgotPassword">
+                        <a className="passwordLink" href="./forgotpassword">Wachtwoord vergeten?</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+
     );
 };
   
