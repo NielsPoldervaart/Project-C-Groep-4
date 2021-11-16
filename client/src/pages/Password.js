cef3e266eb23e98dda2b3b00403c484b5263c223
LuennaCL <luenna.ceri.landman@gmail.com>
2021-11-16 20:26:58 +0100

    passwordshit en registershit toegevoegd

--- client/src/pages/Password.js
+++ client/src/pages/Password.js
@@ -1,6 +1,23 @@
+import './Password.css';
+
 const Password = () => {
     return (
-        <h1>Forgot password</h1>
+        <div className="Password">
+            <div className="kyndaBackground">
+                <div className="gradientWrapper">
+                    <div className="linear-gradient"></div>
+                    <div className="radial-gradient"></div>
+                </div>
+            </div>
+
+            <nav className="header">
+                <a className="headerLink" href="/">
+                    <img src="https://assets.website-files.com/5f5210d08f59cd33456fb659/5f5210d08f59cd67186fb79a_kyndalogo.svg" alt="kyndaLogo" />
+                    <div className="logoBlockStripe"></div>
+                </a>
+                <h1>FORGOT PASSWORD</h1>
+            </nav>
+        </div>
     );
 };
   
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,23 @@
+import './Register.css';
+
 const Register = () => {
     return (
-        <h1>Register</h1>
+        <div className="Register">
+            <div className="kyndaBackground">
+                <div className="gradientWrapper">
+                    <div className="linear-gradient"></div>
+                    <div className="radial-gradient"></div>
+                </div>
+            </div>
+
+            <nav className="header">
+                <a className="headerLink" href="/">
+                    <img src="https://assets.website-files.com/5f5210d08f59cd33456fb659/5f5210d08f59cd67186fb79a_kyndalogo.svg" alt="kyndaLogo" />
+                    <div className="logoBlockStripe"></div>
+                </a>
+                <h1>REGISTER</h1>
+            </nav>
+        </div>
     );
 };
   
