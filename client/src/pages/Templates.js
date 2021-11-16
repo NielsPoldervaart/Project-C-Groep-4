f86f5ba5a4770bc3d685f0f4bcdb9003714a0e9b
NielsPoldervaart <Niels@meghira.nl>
2021-11-17 00:08:51 +0100

    Added basic template card Added css import Added onClick navigation

--- client/src/pages/Templates.js
+++ client/src/pages/Templates.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router';
+import { useNavigate } from 'react-router-dom';
+import '../style/Templates.css';
 
 const Templates = () => {
-    const { company_id } = useParams()
+    let navigate = useNavigate();
 
+    const { company_id } = useParams()
     const [templates, setTemplates] = useState([])
 
     useEffect(() => {
@@ -17,14 +20,18 @@ const Templates = () => {
     }, [company_id])
 
     return (
-        <div>
-            <ul>
+        <div className="Body">
+            <h1 className="CompanyName">Company Name</h1>
+            <ul className="TemplateList">
                 {
                     templates.map((template) => 
-                    
-                        <li key={template.template_id}>
-                            {template.template_id} - {template.company_name} - {template.template_file} 
-                        </li>
+                        <div>
+                            <h2 className="TitleCard">Template {template.template_id}</h2>
+                            <div key={template.template_id} className="TemplateCard">
+                                <p className="CardIcon View" onClick={() => navigate(`/${company_id}/${template.template_id}`)}>V</p>
+                                <p className="CardIcon Delete">D</p>
+                            </div>
+                        </div>
                     )
                 }
             </ul>
