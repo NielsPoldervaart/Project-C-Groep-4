d8fd2ebd3befb04aca5cdd3539fc17ef9ba2c6a0
NielsPoldervaart <Niels@meghira.nl>
2021-11-17 11:46:45 +0100

    Cleaned up App.js to only include Navbar.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from './pages/Login';
-import Template from './pages/Template';
-import Templates from './pages/Templates';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <Link to="/">Tamplates</Link>
-        <Link to="/">Tamplate</Link>
-        <Link to="/login">Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/:company_id" element={<Templates />}/>
-        <Route path="/:company_id/:template_id" element={<Template />}/>
-        <Route path="/login" element={<Login />}/>
-      </Routes>
-    </Router>
+    <div className="App">
+      <Navbar />
+    </div>
   );
 }
 
