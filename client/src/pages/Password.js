b5f42595f95f4c0099e6b6113395cec1f3b363ce
NielsPoldervaart <Niels@meghira.nl>
2021-11-18 21:07:35 +0100

    Removed unnecessary code Fixed some small styling issues

--- client/src/pages/Password.js
+++ client/src/pages/Password.js
@@ -1,22 +1,9 @@
-import './Password.css';
+import '../style/Password.css';
 
 const Password = () => {
     return (
         <div className="Password">
-            <div className="kyndaBackground">
-                <div className="gradientWrapper">
-                    <div className="linear-gradient"></div>
-                    <div className="radial-gradient"></div>
-                </div>
-            </div>
-
-            <nav className="header">
-                <a className="headerLink" href="/">
-                    <img src="https://assets.website-files.com/5f5210d08f59cd33456fb659/5f5210d08f59cd67186fb79a_kyndalogo.svg" alt="kyndaLogo" />
-                    <div className="logoBlockStripe"></div>
-                </a>
-                <h1>FORGOT PASSWORD</h1>
-            </nav>
+            <h1 className="PasswordText">Forgot Password</h1>
         </div>
     );
 };
