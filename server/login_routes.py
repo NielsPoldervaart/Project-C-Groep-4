d7fd50d68025b8bbad20f2d5e1ef7d1cd4fec8ca
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-11-18 23:05:55 +0100

    Added many new files for routes + Blueprints

---
+++ server/login_routes.py
@@ -0,0 +1,62 @@
+from flask import request, session, Blueprint
+from werkzeug.security import generate_password_hash, check_password_hash
+from user_verification import verify_user
+from database_connection import db_connection
+
+login_api = Blueprint('login_api', __name__)
+
+@login_api.route("/login", methods=["POST"])
+def login():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    if request.method == "POST": #haal wachwoord van server voor juiste user
+        inserted_password = request.form["password"]
+        inserted_user_email = request.form["email"]
+
+        sql = f"""Select user_id, company_company_id, role_role_id, password from User where email = "{inserted_user_email}" """
+        cursor.execute(sql)
+        user = cursor.fetchone()
+
+        if user: #IF USER OBJECT IS NOT NONE (COULD FIND CORRECT DATA IN DB)
+            if not check_password_hash(user["password"], inserted_password):
+                print(user["password"], inserted_password)
+                {"Code": 406, "Message": "Incorrect User credentials"""}
+
+            session["user_id"] = user["user_id"]
+            session["company_company_id"] = user["company_company_id"]
+            session["role_role_id"] = user["role_role_id"]
+            return "Succesfully logged in"
+
+        else:
+           return {"Code": 406, "Message": "Incorrect User credentials"""}
+
+@login_api.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    session.pop("company_company_id", None)
+    return {"Code": 201, "Message": "User logged out"""}
+
+@login_api.route("/register/<company_id>", methods = ["POST"])
+def register(company_id):
+    #TODO: COMPANY ID CAN BE RETRIEVED FROM USER DATA IN SESSION
+    user_verification = verify_user(company_id, [1])
+    if user_verification != "PASSED":
+        return user_verification
+
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    if request.method == "POST":
+        inserted_first_name = request.form["first_name"]
+        inserted_last_name = request.form["last_name"]
+        inserted_user_email = request.form["email"]
+        inserted_password = request.form["password"]
+        inserted_role = request.form["role_id"]
+        hashed_password = generate_password_hash(inserted_password)
+
+        sql = f"""INSERT INTO `User` VALUES (default, "{inserted_first_name}", "{inserted_last_name}", "{inserted_user_email}", "{hashed_password}", "{company_id}", "{inserted_role}");"""
+        cursor.execute(sql)
+        conn.commit()
+
+        return {"Code": 201, "Message": "User added to company"""}
