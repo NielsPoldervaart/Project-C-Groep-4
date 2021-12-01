6a7d5bbca357922ab172177a7d8ac105da95cc4f
LuennaCL <luenna.ceri.landman@gmail.com>
2021-12-01 14:16:56 +0100

    changed navbar & homescreen

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,19 +6,29 @@ import Template from '../pages/Template';
 import Templates from '../pages/Templates';
 import Register from '../pages/Register';
 import Password from '../pages/Password';
+import Home from '../pages/Home';
 
 const Navbar = () => {
     return (
         <Router>
             <header className="Navbar">
-                <h1 className="NavLogo"><Link className="Link" to="/1">Kynda</Link></h1>
+                <h1 className="NavLogo"><Link className="Link" to="/1">
+                    <img src="https://excelsiorrotterdam.nl/wp-content/uploads/2018/01/kynda_web.png" alt="KyndaLogo" width="150px"/>
+                    </Link></h1>
                 <nav>
                     <ul className="NavLinks">
                         <li><Link className="Link" to="/1">Tamplates</Link></li>
-                        <li><Link className="Link" to="/login">Login</Link></li>
-                        <li><Link className="Link" to="/register">Register</Link></li>
-                        <li><Link className="Link" to="/forgotpassword">Password</Link></li>
-                        <li><Link className="Link" to="/logout">Logout</Link></li>
+                        <li><Link className="Link" to="/images">Beeldbank</Link></li>
+                        <li><Link className="Link" to="/accounts">Accounts</Link></li>
+                    </ul>
+                    <ul className="profilePic">
+                        <img src="https://image.flaticon.com/icons/png/512/50/50050.png" alt="pfpIcon" width="50px"/>
+                        <div className="profileMenu">
+                            <li><Link className="Link" to="/profile">Profiel</Link></li>
+                            <li><Link className="Link" to="/manual">Handboek</Link></li>
+                            <li><Link className="Link" to="/help">Help</Link></li>
+                            <li><Link className="Link" to="/logout">Uitloggen</Link></li>
+                        </div>
                     </ul>
                 </nav>
             </header>
@@ -29,6 +39,7 @@ const Navbar = () => {
                 <Route path="/register" element={<Register />}/>
                 <Route path="/forgotpassword" element={<Password />}/>
                 <Route path="/logout" element={<Login />}/>
+                <Route path="/welcome" element={<Home />}/>
             </Routes>
         </Router>
         
