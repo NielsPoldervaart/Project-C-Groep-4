f3f7f8058b2291eb72b6628e2656cc241d1c3553
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-12-05 12:50:59 +0100

    Changed template insertion to SQLAlchemy

--- server/template_routes.py
+++ server/template_routes.py
@@ -12,13 +12,10 @@ def templates(company_identifier):
     if user_verification != "PASSED":
         return user_verification
 
-    #conn = db_connection()
-    #cursor = conn.cursor()
-
     db_session = create_db_session()
 
     if request.method == "GET": #View ALL templates from a company
-
+        #SELECT `Template_id`, `Template_file`, Company_name WHERE `Company_1` = company_identifier
         result = db_session.query(Template.template_id, Template.template_file, Company.company_name).join(Company).filter_by(company_id = f'{company_identifier}').all()
 
         templates = [
@@ -38,12 +35,12 @@ def templates(company_identifier):
         #TODO: ACTUAL TEMPLATE NEEDS TO BE ADDED TO STORAGE
 
         new_template_path = request.form["template_path"]
+        #New Template object is created, None is used for id as it is auto-incremented by SQLAlchemy
+        new_template = Template(None, new_template_path, company_identifier)
 
-        sql = """INSERT INTO `Template` (Template_id, Template_file, Company_company_id)
-                    Values(default, %s, %s)
-        """
-        cursor = cursor.execute(sql, (new_template_path, company_id))
-        conn.commit()
+        db_session.add(new_template)
+        db_session.commit()
+
         return {"Code": 201, "Message": "Template added to company"""}
 
 @template_api.route("/template/<company_id>/<template_id>", methods=["GET", "DELETE"])
