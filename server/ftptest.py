ca67ec6584faebde15b133d58c7208774375cb06
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-12-06 19:41:51 +0100

    FTP tests

--- server/ftptest.py
+++ server/ftptest.py
@@ -1,5 +1,37 @@
 from ftplib import FTP
 
-ftp = FTP('145.24.222.235')
-ftp.login("Controller", "cC2G'Q_&3qY@=D!@")
-ftp.dir()
+session = FTP('145.24.222.235')
+session.login("Controller", "cC2G'Q_&3qY@=D!@")
+
+contents = []
+filename = 'test.html'
+
+def getFile(filename, company_id=0):
+    global contents
+    session.retrlines("RETR " + filename, collectLines)
+    tempContents = contents
+    contents = []
+    return tempContents
+
+def uploadFile():
+    pass
+
+def updateFile():
+    pass
+
+def deleteFile():
+    pass
+
+def createDir():
+    pass
+
+def deleteDir():
+    pass
+
+def collectLines(s):
+    global contents
+    contents.append(s)
+
+getFile(filename)
+print(contents)
+#ftp.dir()
