bca87368602d248865dcce45c61d1578a165deb0
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2021-12-14 16:20:09 +0100

    Added GET routes for image_routes

---
+++ server/image_routes.py
@@ -0,0 +1,106 @@
+from flask import Blueprint, request, jsonify, send_from_directory, send_file
+from user_verification import verify_user
+from ftp_controller import get_file_full, upload_file
+from database_connection import *
+import os
+from os import path
+from generate_random_path import generate_random_path
+
+image_api = Blueprint('image_api', __name__)
+
+@image_api.route("/images/<company_identifier>", methods=["GET","POST"])
+def galleries(company_identifier):
+
+    user_verification = verify_user(company_identifier)
+    if user_verification != "PASSED":
+        return user_verification
+
+    db_session = create_db_session()
+    
+    if request.method == "GET": #View all galleries available to the company
+        result = db_session.query(Gallery.gallery_id, Gallery.name, Company.company_id).join(Gallery_has_Company).filter_by(gallery_id = Gallery_gallery_id).join(Company).filter_by(Company_company_id = f'{company_identifier}').all()
+        galleries = [
+            dict(
+                gallery_id = row['gallery_id'],
+                gallery_name = row['name'],
+                company_id = row['company_id']
+            )
+            for row in result
+        ]
+        if len(galleries) is not 0:
+            return jsonify(galleries)
+        else:
+            return {"errorCode": 404, "Message": "There are no galleries available"""}
+    if request.method == "POST": #Add a new gallery (when user is Kynda employee)
+        return
+
+@image_api.route("/images/<company_identifier>/<gallery_identifier>", methods=["GET","POST","DELETE"])
+def collections(company_identifier,gallery_identifier):
+
+    user_verification = verify_user(company_identifier)
+    if user_verification != "PASSED":
+        return user_verification
+
+    db_session = create_db_session()
+    
+    if request.method == "GET": #View all collections inside the gallery
+        result = db_session.query(Collection.collection_id, Collection.name).join(Collection).filter_by(Gallery_gallery_id = f'{gallery_identifier}').all()
+        collections = [
+            dict(
+                collection_id = row['collection_id'],
+                name = row['name']
+            )
+            for row in result
+        ]
+        if len(collections) is not 0:
+            return jsonify(collections)
+        else:
+            return {"errorCode": 404, "Message": "There are no collections in this gallery"""}
+    if request.method == "POST": #Add a new collection (when user is Kynda employee)
+        return
+    if request.method == "DELETE": #Remove the gallery (when user is Kynda employee)
+        return
+
+@image_api.route("/images/<company_identifier>/<gallery_identifier>/<collection_identifier>", methods=["GET","POST","DELETE"])
+def images(company_identifier,gallery_identifier,collection_identifier):
+    user_verification = verify_user(company_identifier)
+    if user_verification != "PASSED":
+        return user_verification
+
+    db_session = create_db_session()
+    
+    if request.method == "GET": #View all images inside the collection
+        result = db_session.query(Image.image_id, Image.image_path).join(Image_has_Collection).filter_by(Collection_collection_id = f'{collection_identifier}').filter_by(Image_id = Image_image_id).all()
+        images = [
+            dict(
+                image_id = row['image_id'],
+                image_path = row['image_path']
+            )
+            for row in result
+        ]
+        if len(images) is not 0:
+            return jsonify(images)
+        else:
+            return {"errorCode": 404, "Message": "There are no images in this collection"""}
+    if request.method == "POST": #Add a new image (when user is Kynda employee)
+        return
+    if request.method == "DELETE": #Remove the collection (when user is Kynda employee)
+        return
+
+@image_api.route("/images/<company_identifier>/<gallery_identifier>/<collection_identifier>/<image_identifier>", methods=["GET","DELETE"])
+def image(company_identifier,gallery_identifier,collection_identifier,image_identifier):
+
+    user_verification = verify_user(company_identifier)
+    if user_verification != "PASSED":
+        return user_verification
+
+    db_session = create_db_session()
+    
+    if request.method == "GET": #View the image
+        result = db_session.query(Image.image_id, Image.image_path).filter_by(image_id = f'{image_identifier}').first()
+        if result is not None:
+            return jsonify(dict(image_id = row['image_id'], image_path = row['image_path']) for row in result)
+        else:
+            return {"errorCode": 404, "Message": "This image does not exist"""}
+    if request.method == "DELETE": #Remove the image (when user is Kynda employee)
+        return
--- server/server.py
+++ server/server.py
@@ -3,11 +3,13 @@ from login_routes import login_api
 from template_routes import template_api
 from company_routes import company_api
 from database_connection import init_db_structure
+from image_routes import image_api
 
 app = Flask(__name__)
 app.register_blueprint(login_api)
 app.register_blueprint(template_api)
 app.register_blueprint(company_api)
+app.register_blueprint(image_api)
 app.secret_key = "ToBeSecret" #TODO: Make Secret key actually secret
 
 if __name__ == "__main__":
