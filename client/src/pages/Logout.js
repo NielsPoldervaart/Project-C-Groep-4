1022f936ba00992b5efe07afc17a6b5d34adcd04
NielsPoldervaart <Niels@meghira.nl>
2021-12-14 15:16:32 +0100

    Changed Logout behaviour

--- client/src/pages/Logout.js
+++ client/src/pages/Logout.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        fetch(`/logout`).then(
-            res => res.json()
-          ).then(
-            data => {
-              console.log(data)
+  useEffect(() => {
+    fetch(`/logout`)
+    .then(res => res.json())
+    .then(
+        data => {
+            if (data.Code === 201) {
+                navigate('/login')
+                console.log("Logout succesful!")
             }
-          )
-    }, [])
+        }
+    ).catch(error => console.log('Logout failed : ' + error.message));
+  }, [])
 
     return (
         <div>
