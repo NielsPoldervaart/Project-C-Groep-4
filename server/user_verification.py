f580916863b9b840d51be3491582fd8108da841f
SebastiaanBooman <iMeowTwitch@gmail.com>
2021-12-17 08:44:49 +0100

    Added proper response codes, postman tests updated

--- server/user_verification.py
+++ server/user_verification.py
@@ -8,17 +8,17 @@ def verify_user(company_id, accepted_roles=[1,2,3]):
 
     #VERIFY IF USER IN SESSION (LOGGED IN)
     if "user_id" and "company_company_id" not in session:
-        return {"errorCode" : 403, "Message" : "Login not authorized"}
+        return {"errorCode" : 401, "Message" : "Login not authorized"}, 401
 
     #VERIFY IF USER IN COMPANY
     company_company_id = session["company_company_id"]
     if int(company_company_id) != int(company_id):
-        return {"errorCode" : 403, "Message" : "Company not within User's companies"}
+        return {"errorCode" : 403, "Message" : "Company not within User's companies"}, 403
 
     #VERIFY IF USER HAS RIGHT ROLE
     role_role_id = session["role_role_id"]
     if int(role_role_id) not in accepted_roles:
-        return {"errorCode" : 403, "Message" : "Required Role not within User's roles"}
+        return {"errorCode" : 403, "Message" : "Required Role not within User's roles"}, 403
 
     #PASSED ALL CHECKS
     return "PASSED"
