899bcea86d1d89c4209de7abb534cfd94d40c53e
LuennaCL <luenna.ceri.landman@gmail.com>
2021-12-29 11:03:47 +0100

    MANUAL WITHOUT URL

--- client/src/pages/Manual.js
+++ client/src/pages/Manual.js
@@ -4,8 +4,7 @@ const Manual = () => {
     return (
         <div className="Manual">
             <h1 className="ManualText">[ MANUAL - WORK IN PROGRESS ]</h1>
-            <iframe src="" frameborder="0"></iframe>
-            <div dangerouslySetInnerHTML={{__html: '/Huisstijlhandboek_Kynda.html'}} />
+            <iframe src="{URL KYNDA HANDBHOEK HTML}" frameborder="0"></iframe>
         </div>
     )
 };
