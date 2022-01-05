5d8c36116f50df0e626c2afc07f230d57304dfa9
LuennaCL <luenna.ceri.landman@gmail.com>
2022-01-05 11:42:14 +0100

    Accounts page

--- client/src/pages/Accounts.js
+++ client/src/pages/Accounts.js
@@ -1,9 +1,41 @@
+import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
 import '../style/Accounts.css'
+import Loader from '../components/Loader';
 
 const Accounts = () => {
+    let navigate = useNavigate();
+
+    const [templateData, setTemplateData] = useState("")
+    const [loading, setLoading] = useState(true);
+
+    const [values, setValues] = useState({
+        accepted: false,
+    });
+
+    const HandleAccept = () => {
+        setValues({...values, accepted: true})
+    }
+
+    const HandleDecline = () => {
+        setValues({...values, accepted: false})
+        //TODO: delete declined account from database
+    }
+
+    const DisplayLoader = () => {
+        return (
+          <div className='loaderDiv'>
+            <Loader />
+          </div>
+        )
+    }
+
     return (
         <div className="Accounts">
-            <h1 className="AccountsText">[ ACCOUNTS - WORK IN PROGRESS ]</h1>
+            <div className="AccountsText">
+                {/* <iframe src="../templates/template1.html" className="LoadedTemplate"/> */}
+                {loading ? DisplayLoader() : <div className='loadedAccounts' dangerouslySetInnerHTML={{__html: templateData}}/>}
+            </div>
         </div>
     )
 };
