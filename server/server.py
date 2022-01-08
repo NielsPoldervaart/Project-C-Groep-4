3b4a81b787af564afdca1a54e5d18d6dd004eee2
Sebastiaan Booman <iMeowTwitch@gmail.com>
2022-01-08 15:19:03 +0100

    Added tests, fixes

--- server/server.py
+++ server/server.py
@@ -19,8 +19,8 @@ app.register_blueprint(init_api)
 app.secret_key = "ToBeSecret" #TODO: Make Secret key actually secret
 
 app.config["TEST_DATABASE_FILENAME"] = "test_sqlite.db"
-app.config["DATABASE_URI"] = "mysql+mysqldb://kynda:u9N3_HM+ARhDYsRQ@kynda-database.cgmcelrbhqyr.eu-west-2.rds.amazonaws.com/KyndaDB" #PROD DB CONNECTION
-#app.config["DATABASE_URI"] = "sqlite:///" + f"{get_absolute_path(app.config['TEST_DATABASE_FILENAME'])}" #TEST DB CONNECTION
+#app.config["DATABASE_URI"] = "mysql+mysqldb://kynda:u9N3_HM+ARhDYsRQ@kynda-database.cgmcelrbhqyr.eu-west-2.rds.amazonaws.com/KyndaDB" #PROD DB CONNECTION
+app.config["DATABASE_URI"] = "sqlite:///" + f"{get_absolute_path(app.config['TEST_DATABASE_FILENAME'])}" #TEST DB CONNECTION
 
 if __name__ == "__main__":
     close_current_sessions()
